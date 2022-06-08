5d55efa16a3bc8d53d6827f782f8c7556e76a929
ryandefea <ryan.defea@gmail.com>
2022-06-07 18:55:28 -0700

    refactor js
    Co-authored-by: Sandra Tena <sandratena06@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
+
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -14,7 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -26,26 +33,28 @@ function numberOfOccurrencesInText(word, text) {
   });
   return wordCount;
 }
-function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
-  let wordCount = 0;
-  let offensiveWord = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
-  let censoredWord = "censored Word";
-  wordArray.forEach(function(element, index) {
-    if (element.includes(word)) {
-      console.log(wordArray);
-      wordArray[index] = censoredWord;
-    }
-  })
-  return wordArray;
-};
-
 
 // UI Logic
-    
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
+
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
     event.preventDefault();
@@ -55,5 +64,22 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   let offensiveWord = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+//   let censoredWord = "censored Word";
+//   wordArray.forEach(function(element, index) {
+//     if (element.includes(word)) {
+//       console.log(wordArray);
+//       wordArray[index] = censoredWord;
+//     }
+//   })
+//   return wordArray;
+// };
